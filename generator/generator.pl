38e8eb459ffb7b466e40e007960a7e8a89988c81
Adland Lee <adland@4mfd.com>
2014-03-15 18:57:42 -0700

    add import export options
    - add import markov model data
    - add import state table data
    - add state export emission only
    - add state export all

--- generator/generator.pl
+++ generator/generator.pl
@@ -8,8 +8,17 @@
 #
 
 use Data::Dumper;
+use Getopt::Long;
 use JSON;
 
+my $all;
+my $markov_model_file;
+my $state_table_file;
+GetOptions(
+	"all" => \$all
+	, "markov-model=s" => \$markov_model_file
+	, "state-table=s" => \$state_table_file
+	);
 
 
 ##############################################################################
@@ -20,7 +29,7 @@ use JSON;
 
 sub import_markov_model {
 	my $filename = shift;
-	my $markov_model = {};
+	my $markov_model;
 
 	open(FH, "<", $filename) or die "< $filename: cannot open $!";
 
@@ -30,6 +39,28 @@ sub import_markov_model {
 	$markov_model = from_json($input);
 
 	return $markov_model;
+}
+
+
+
+##############################################################################
+#
+# Import State Table
+#
+##############################################################################
+
+sub import_state_table {
+	my $filename = shift;
+	my $state_table;
+
+	open(FH, "<", $filename) or die "< $filename: cannot open $!";
+
+	local $/;
+	my $input = <FH>;
+
+	$state_table = from_json($input);
+
+	return $state_table;
 }
 
 
@@ -125,10 +156,34 @@ sub generate_states
 #
 ##############################################################################
 
-my $markov_model = import_markov_model('markov_model.data');
-my $states =  generate_states(
-	'markov_model' => $markov_model
-	, 'count' => 100
-	);
+die "markov_model xor state_table"
+	if (!($markov_model_file xor $state_table_file));
 
-print Dumper $states;
+# print "MM [$markov_model_file]\n";
+
+my $markov_model;
+my $states;
+
+if ($markov_model_file) {
+	$markov_model = import_markov_model($markov_model_file);
+	$states =  generate_states(
+		'markov_model' => $markov_model
+		, 'count' => 100
+		);
+}
+if ($state_table_file) {
+	$states = import_state_table($state_table_file);
+}
+
+# print Dumper $states;
+
+my $output;
+
+if ($all) {
+	print to_json $states;
+}
+else {
+	@{$output} = map { $_->{'emission'} } @{$states};
+	# print Dumper $output;
+	print to_json($output);
+}
